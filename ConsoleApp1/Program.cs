4aa036f0a1cb3a881515bc46eb64f5f2ed61082c
Jasper <guitar1859@gmail.com>
2022-11-11 16:14:35 +0800

    加入專案檔案。

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using iSpan.Utility;
+
+namespace ConsoleApp1
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			string insertSql = @"INSERT into Users(Name, Account, Password, DateOfBirthd, Height) 
+Values(@Name, @Account,@password,@DateOfBirthd,@Height) ";
+
+            string updateSql = @"UPDATE Users SET Name=@Name, Account=@Account ,
+Password=@password , DateOfBirthd=@DateOfBirthd , Height =@Height where id = @id";
+
+            string deleteSql = @"DELETE FROM Users WHERE id = @id ";
+
+            string selectSql = @"SELECT Id, Name, Account, Password, DateOfBirthd, Height FROM Users";
+
+            //SqlParameter[] parameters = new sqlParameterBuilder()
+            //.AddNvarchar("Name",50,"t4")
+            //.AddNvarchar("Account", 50,"t4account")
+            //.AddInt("Password", 123)
+            //.AddDateTime("DateOfBirthd", "2022/8/7")
+            //.AddInt("Height",111)
+            //.AddInt("id",4)
+            //.Build();
+            var dbHelper = new sqlDbHelper("default");
+            //dbHelper.Update(updateSql, parameters);
+
+            #region 使用select時
+            try
+            {
+                var parameters = new sqlParameterBuilder().AddInt("id", 0).Build();
+                DataTable news = dbHelper.Select(selectSql, parameters);
+                foreach (DataRow row in news.Rows)
+                {
+                    int id = row.Field<int>("Id");
+                    string name = row.Field<string>("name");
+                    Console.WriteLine($"Id={id}, Name={name}");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"連線失敗, 原因 :{ex.Message}");
+            }
+            #endregion
+
+        }
+    }
+}
---
+++ iSpan.Utility/sqlParameterBuilder.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace iSpan.Utility
+{
+	public class sqlParameterBuilder
+	{
+
+		//public SqlParameter Insert()
+		//{
+		//}
+		private List<SqlParameter> parameters = new List<SqlParameter>();
+		public sqlParameterBuilder AddNvarchar(string name, int length, string value)
+		{
+			SqlParameter param = new SqlParameter(name, SqlDbType.NVarChar, length) { Value = value};
+			parameters.Add(param);
+			return this;
+		}
+		public sqlParameterBuilder AddInt(string name, int value)
+		{
+			SqlParameter param = new SqlParameter(name, SqlDbType.Int) { Value = value };
+			parameters.Add(param);
+			return this;
+		}
+		public sqlParameterBuilder AddDateTime(string name, string value)
+		{
+			SqlParameter param = new SqlParameter(name, SqlDbType.DateTime) { Value = Convert.ToDateTime(value)};
+			parameters.Add(param);
+			return this;
+		}
+		public SqlParameter[] Build()
+		{
+			return parameters.ToArray();
+		}
+	}
+}
